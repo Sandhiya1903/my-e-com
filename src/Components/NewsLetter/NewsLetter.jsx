818c622adba9acb3e446e12c193bd98eae20613a
Sandhiya1903 <sandhiyaelumalai19@gmail.com>
2024-02-12 14:36:09 +0530

    sss

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NewsLetter.css'
+function NewsLetter() {
+  return (
+    <div className='newsletter'>
+      <h1>Get Exclusive Offers On Your Emails </h1>
+      <p>subscribe to our newsletter and stay updated</p>
+    <div>
+        <input type='email' placeholder='your email id'/>
+        <button>Subscribe</button>
+    </div>
+
+    </div>
+  )
+}
+
+export default NewsLetter
